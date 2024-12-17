0517870e8d8c1f7319aacf3c34d359ab3aaf318d
Dmitriy-68 <d73-68@yandex.ru>
2024-12-17 11:48:24 +0300

    Модуль 19 Задание 4 Сайт администрирования

--- PythonDjango/task1/admin.py
+++ PythonDjango/task1/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Buyer, Game
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size',)
+    list_filter = ('cost', 'size',)
+    search_fields = ('title',)
+    list_per_page = 20
+
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age',)
+    list_filter = ('balance', 'age',)
+    search_fields = ('name',)
+    list_per_page = 30
+    readonly_fields = ('balance',)
